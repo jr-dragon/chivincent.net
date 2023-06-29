c96861d6ae6a148a2124f265fa24f8c9a57c4439
Vincent Chi <song374561@chivincent.net>
2023-06-30 01:44:35 +0800

    Update Algolia configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,29 +120,12 @@ const config = {
         appId: 'SANPS1NFCM',
   
         // Public API key: it is safe to commit it
-        apiKey: '0f2010eb37369f31e12950c60e8ff1a9',
+        apiKey: 'd44c63dce57f1cc3c3f0abd0218e39f6',
   
         indexName: 'chivincent',
   
         // Optional: see doc section below
-        contextualSearch: true,
-  
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
-  
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: '/docs/', // or as RegExp: /\/docs\//
-          to: '/',
-        },
-  
-        // Optional: Algolia search parameters
-        searchParameters: {},
-  
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-  
-        //... other Algolia params
+        contextualSearch: false,
       },
     }),
 };
